af087020905457ddece769bce6a041f6510d9219
Zkrallah <muhammad.heshamyt@gmail.com>
2024-05-03 05:23:15 +0300

    DFS Solution

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+visited = set()
+
+def canUnlockAll(boxes) :
+    visited.clear()
+    dfs(boxes, 0)
+
+    if len(visited) == len(boxes):
+        return True
+
+    return False
+
+def dfs(boxes, currIdx) :
+    visited.add(currIdx)
+    box = boxes[currIdx]
+
+    for key in box:
+        if key not in visited:
+            dfs(boxes, key)
