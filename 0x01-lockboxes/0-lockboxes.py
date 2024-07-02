e8c340ac964e7d18c5d7019e23f2f1aa0039faaf
Zkrallah <muhammad.heshamyt@gmail.com>
2024-07-03 02:37:15 +0300

    My Answer

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
-'''A module for working with lockboxes.
-'''
+'''Lockboxes DFS Solution'''
+visited = set()
 
 
 def canUnlockAll(boxes):
-    '''Checks if all the boxes in a list of boxes containing the keys
-    (indices) to other boxes can be unlocked given that the first
-    box is unlocked.
-    '''
-    n = len(boxes)
-    seen_boxes = set([0])
-    unseen_boxes = set(boxes[0]).difference(set([0]))
-    while len(unseen_boxes) > 0:
-        boxIdx = unseen_boxes.pop()
-        if not boxIdx or boxIdx >= n or boxIdx < 0:
-            continue
-        if boxIdx not in seen_boxes:
-            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
-            seen_boxes.add(boxIdx)
-    return n == len(seen_boxes)
+    '''Setup Function'''
+    visited.clear()
+    dfs(boxes, 0)
+
+    if len(visited) == len(boxes):
+        return True
+
+    return False
+
+
+def dfs(boxes, currIdx):
+    '''Dfs Function'''
+    if currIdx > len(boxes):
+        return
+
+    visited.add(currIdx)
+    box = boxes[currIdx]
+
+    for key in box:
+        if key not in visited:
+            dfs(boxes, key)
