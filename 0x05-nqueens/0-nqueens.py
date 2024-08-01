ec27ac3c8d5420508050236e81e51800143ec9e0
Zkrallah <muhammad.heshamyt@gmail.com>
2024-08-01 16:19:54 +0300

    NQueens Backtracking Solution.

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -77,19 +77,4 @@ def solve(n):
         print(i)
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./0-nqueens.py N")
-        sys.exit(1)
-
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
 solve(n)
