79d087e72e2d291c9bbfa93842e1cc8fb82e16b3
Zkrallah <muhammad.heshamyt@gmail.com>
2024-08-08 18:41:44 +0300

    Styling shit.

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,13 +5,15 @@ const request = require('request');
 const base_url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
 request(base_url, function (error, response, body) {
-  if (error) throw error;
+  if (err) throw err;
   const characters = JSON.parse(body).characters;
-  
-  for (const character of characters) {
-    request(character, function (err, res, bd) {
-      if (err) throw err;
-      console.log(JSON.parse(bd).name);
-    });
-  }
+  loop(characters, 0);
 });
+const loop = (characters, x) => {
+  if (x === characters.length) return;
+  request(characters[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    loop(characters, x + 1);
+  });
+};
