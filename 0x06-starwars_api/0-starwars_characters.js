16b0398c19ebd2dc546f0108302b08cca442b85e
Zkrallah <muhammad.heshamyt@gmail.com>
2024-08-08 18:32:17 +0300

    Missed shebang

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,3 +1,5 @@
+#!/usr/bin/node
+
 const request = require('request');
 
 const base_url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
@@ -11,4 +13,4 @@ request(base_url, function (error, response, body)  {
         console.log(JSON.parse(bd).name);
       });
     }
-  });
+});
